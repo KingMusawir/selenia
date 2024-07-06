4bee8a1037012c5c3f7bc3de12a2b14009bc3de4
KingMusawir <kingola23@gmail.com>
2024-07-06 04:47:56 -0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-slick', 'slick-carousel'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-slick'],
-    },
-  },
 });
